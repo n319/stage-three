c642c5da710c4bac15aedc0128373fde6aba9852
Nick Smith <nsc319@gmail.com>
2019-03-19 04:25:02 -0400

    added auth controller method

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -1,76 +1,80 @@
 using System.Collections.Generic;
 using AH.Api.Models;
 using AH.Api.Services;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
-namespace AH.Api.Controllers
-{
-    [Route("api/[controller]")]
+namespace AH.Api.Controllers {
+    [Authorize]
+    [Route ("api/[controller]")]
     [ApiController]
-    public class UserController : ControllerBase
-    {
+    public class UserController : ControllerBase {
         private readonly UserService _userService;
 
-        public UserController(UserService userService)
-        {
+        public UserController (UserService userService) {
             _userService = userService;
         }
 
-        [HttpGet]
-        public ActionResult<List<AgileHouseUser>> Get()
+        [AllowAnonymous]
+        [HttpPost("authenticate")]
+        public IActionResult Authenticate([FromBody]AgileHouseUser userParam)
         {
-            return _userService.Get();
-        }
-
-        [HttpGet("{id:length(24)}", Name = "GetUser")]
-        public ActionResult<AgileHouseUser> Get(string id)
-        {
-            var user = _userService.Get(id);
+            var user = _userService.Authenticate(userParam.Username, userParam.Password);
 
             if (user == null)
-            {
-                return NotFound();
+                return BadRequest(new { message = "Username or password is incorrect" });
+
+            return Ok(user);
+        }
+
+
+        [HttpGet]
+        public ActionResult<List<AgileHouseUser>> Get () {
+            return _userService.Get ();
+        }
+
+        [HttpGet ("{id:length(24)}", Name = "GetUser")]
+        public ActionResult<AgileHouseUser> Get (string id) {
+            var user = _userService.Get (id);
+
+            if (user == null) {
+                return NotFound ();
             }
 
             return user;
         }
 
         [HttpPost]
-        public ActionResult<AgileHouseUser> Create(AgileHouseUser user)
-        {
-            _userService.Create(user);
+        public ActionResult<AgileHouseUser> Create (AgileHouseUser user) {
+            _userService.Create (user);
 
-            return CreatedAtRoute("GetUser", new { id = user.Id.ToString() }, user);
+            return CreatedAtRoute ("GetUser", new { id = user.Id.ToString () }, user);
         }
 
-        [HttpPut("{id:length(24)}")]
-        public IActionResult Update(string id, AgileHouseUser userIn)
-        {
-            var user = _userService.Get(id);
+        [HttpPut ("{id:length(24)}")]
+        public IActionResult Update (string id, AgileHouseUser userIn) {
+            var user = _userService.Get (id);
 
-            if (user == null)
-            {
-                return NotFound();
+            if (user == null) {
+                return NotFound ();
             }
 
-            _userService.Update(id, userIn);
+            _userService.Update (id, userIn);
 
-            return NoContent();
+            return NoContent ();
         }
 
-        [HttpDelete("{id:length(24)}")]
-        public IActionResult Delete(string id)
-        {
-            var user = _userService.Get(id);
+        [HttpDelete ("{id:length(24)}")]
+        public IActionResult Delete (string id) {
+            var user = _userService.Get (id);
 
-            if (user == null)
-            {
-                return NotFound();
+            if (user == null) {
+                return NotFound ();
             }
 
-            _userService.Remove(user);
+            _userService.Remove (user);
 
-            return NoContent();
+            return NoContent ();
         }
     }
 }
