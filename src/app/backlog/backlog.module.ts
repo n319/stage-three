9a58df5040b2d361f71efe58e9e376735c2d166c
Nick Smith <nsc319@gmail.com>
2019-04-20 02:51:44 -0400

    wired up component routing

--- src/app/backlog/backlog.module.ts
+++ src/app/backlog/backlog.module.ts
@@ -2,11 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BacklogHomeComponent } from './backlog-home/backlog-home.component';
 import { PinboardComponent } from './pinboard/pinboard.component';
+import { BacklogRoutingModule } from './backlog-routing.module';
+import { ArchiveComponent } from './archive/archive.component';
 
 @NgModule({
-  declarations: [BacklogHomeComponent, PinboardComponent],
+  declarations: [BacklogHomeComponent, PinboardComponent, ArchiveComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    BacklogRoutingModule
   ]
 })
 export class BacklogModule { }
