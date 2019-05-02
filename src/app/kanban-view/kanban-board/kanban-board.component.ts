490c75631ddf82efc94293bace46f48302bad6ad
Nick Smith <nsc319@gmail.com>
2019-05-01 22:55:01 -0400

    abstract base view class over content service to share code across views

---
+++ src/app/kanban-view/kanban-board/kanban-board.component.ts
@@ -0,0 +1,10 @@
+import { Component, OnInit } from '@angular/core';
+import { PanelViewComponent } from '@app/base-view/panel-view.component';
+
+@Component({
+  selector: 'app-kanban-board',
+  templateUrl: './kanban-board.component.html',
+  styleUrls: ['./kanban-board.component.scss']
+})
+export class KanbanBoardComponent extends PanelViewComponent{
+}
---
+++ src/app/kanban-view/kanban-view.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { KanbanBoardComponent } from './kanban-board/kanban-board.component';
+import { CardDetailComponent } from './card-detail/card-detail.component';
+import { CardSummaryComponent } from './card-summary/card-summary.component';
+import { ContextMenuComponent } from './context-menu/context-menu.component';
+import { KanbanListComponent } from './kanban-list/kanban-list.component';
+import { BaseViewModule } from '@app/base-view/base-view.module';
+import { SharedModule } from '@app/shared';
+
+@NgModule({
+  declarations: [KanbanBoardComponent, CardDetailComponent, CardSummaryComponent, ContextMenuComponent, KanbanListComponent],
+  imports: [
+    CommonModule,
+    BaseViewModule,
+    SharedModule
+  ]
+})
+export class KanbanViewModule { }
