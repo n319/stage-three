cc13c6c723da8ec53dd9b9bb4dce7c82ad5c47a5
Nick Smith <nsc319@gmail.com>
2019-05-03 00:41:04 -0400

    began wiring backlog, archive, and pinboard modules together. tweaked the base view componets for cleaner inheritance

---
+++ src/app/archive-view/archive-board/archive-board.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+import { PanelViewComponent } from '@app/base-view/panel-view.component';
+
+@Component({
+  selector: 'app-archive-board',
+  templateUrl: './archive-board.component.html',
+  styleUrls: ['./archive-board.component.scss']
+})
+export class ArchiveBoardComponent extends PanelViewComponent implements OnInit {
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/archive-view/archive-view.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ArchiveBoardComponent } from './archive-board/archive-board.component';
+import { BaseViewModule } from '@app/base-view/base-view.module';
+import { SharedModule } from '@app/shared';
+
+@NgModule({
+  declarations: [ArchiveBoardComponent],
+  imports: [
+    CommonModule,
+    BaseViewModule,
+    SharedModule
+  ],
+  exports: [ArchiveBoardComponent]
+})
+export class ArchiveViewModule { }
